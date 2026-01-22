03dbccc81b43d2dfce988539b696f48e6d4fcd75
Saif Ur Rehman <saifizaki456@gmail.com>
2026-01-22 19:53:56 +0000

    files added

--- modules/iam/outputs.tf
+++ modules/iam/outputs.tf
@@ -1,6 +1,17 @@
-# Outputs
-output "iam_user_name" {
+output "aws_iam_user" {
   value = aws_iam_user.user.name
+}
+
+output "aws_iam_group" {
+  value = aws_iam_group.group.name
+}
+
+output "aws_iam_role" {
+  value = aws_iam_role.ec2_role.name
+}
+
+output "ec2_instance_profile" {
+  value = aws_iam_instance_profile.ec2_instance_profile.name
 }
 
 output "access_key_id" {
@@ -12,8 +23,3 @@ output "secret_access_key" {
   value     = aws_iam_access_key.access_key.secret
   sensitive = true
 }
-
-output "ec2_instance_profile" {
-  value = aws_iam_instance_profile.ec2_instance_profile.name
-}
-
